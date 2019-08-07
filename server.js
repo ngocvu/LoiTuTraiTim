54701ccc93f8894686861131cbae1acc5af972b9
GauTuyet <53827408+GauTuyet@users.noreply.github.com>
2019-08-07 07:25:58 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const bot = new Discord.Client();
 const fs = require('fs');
 const newUsers = [];
 
+let userData = JSON.parse(fs.readFileSync('userData.json', 'utf8'));
+
 const token = 'NjA4MTk2Nzc4NjEyNzUyMzk1.XUkvkQ.l2998Alja9jwZGBjWJvo_rmuwoE';
 
 bot.on('ready', () =>{
@@ -12,13 +14,27 @@ bot.on('ready', () =>{
 
 bot.on("guildMemberAdd", (member,message) => {
   let memberid = member.user.id;
-  member.guild.channels.get('608132630298230842').send("Ch‡o <@"+  memberid +"> , Ch‡o b?n d?n v?i server L?i T? Tr·i Tim, b?n nÍn v‡o #rules-:warning:  d? d?c lu?t tru?c khi vui choi v‡o c?ng d?ng n‡y ^^. Xong r?i v‡o #:two_hearts:-l?i-t?-tr·i-tim-:two_hearts:  d? trÚ chuy?n nhÈ.")
+  member.guild.channels.get('608132630298230842').send("Ch√†o <@"+  memberid +"> , Ch√†o b?n d?n v?i server L?i T? Tr√°i Tim, b?n n√™n v√†o #rules-:warning:  d? d?c lu?t tru?c khi vui choi v√†o c?ng d?ng n√†y ^^. Xong r?i v√†o #:two_hearts:-l?i-t?-tr√°i-tim-:two_hearts:  d? tr√≤ chuy?n nh√©.")
 }); 
 
-bot.on('message', msg=>{
+bot.on('message', message=>{
+	let sender = message.author;
+	let msg = message.content.toUpperCase();
+	
 	if (msg.content === 'boh ping' || msg.content === 'bohping') {
 		msg.channel.send('Pong! :ballot_box_with_check:')
 	}
+	
+	if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
+	if (!userData[sender.id | message.guild.id].money) userData[sender.id | message.guild.id].money = 1000;
+	
+	fs.writeFile('userData.json',JSON.stringify(userData), (user) => {
+		if (err) console.error(err);
+	})
+})
+
+bot.on('ready', () => {
+	console.log('Kh·ªüi ch·∫°y ti·ªÅn t·ªá...');	
 })
 
 bot.login(token);
