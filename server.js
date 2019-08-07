206e8e0b837fb20c036b78a79e3e62714ace6352
GauTuyet <53827408+GauTuyet@users.noreply.github.com>
2019-08-07 08:48:23 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // JavaScript source code
 const Discord = require('discord.js');
+const eco = require("./discordeco.js");
 const bot = new Discord.Client();
-const fs = require('fs');
+
 const newUsers = [];
 
 let userData = JSON.parse(fs.readFileSync('userData.json', 'utf8'));
@@ -27,21 +28,63 @@ bot.on('message', message=>{
 		msg.channel.send('Pong! :ballot_box_with_check:')
 	}
 	
-	if (!userData[sender.id + message.guild.id]) userData[sender.id + message.guild.id] = {}
-	if (!userData[sender.id | message.guild.id].money) userData[sender.id | message.guild.id].money = 1000;
 	
 	fs.writeFile('userData.json',JSON.stringify(userData), (user) => {
 		if (err) console.error(err);
 	})
 	
 	if (msg.content === 'bohmoney' || msg.content === 'boh money'){
-		let value= userData[sender.id + message.guild.id].money;
-		message.channel.send('Bạn đang giữ '+ value +' boh'); 
+		var output = await eco.FetchBalance(message.author.id)
+    		message.channel.send(`${message.author.tag} đang sở hữu ${output.balance} boh.`);
 	}
+	
+	  if (command === 'daily') {
+ 
+    		var output = await eco.Daily(message.author.id)
+ 
+    		if (output.updated) {
+ 
+      		var profile = await eco.AddToBalance(message.author.id, 1000)
+      		message.reply(`Bạn đã nhận daily thành công! Bây giờ, bạn đang có ${profile.newbalance} boh.`);
+ 
+    		} else {
+      		message.channel.send(`Xin lỗi, bạn đã nhận daily rồi\nNhưng đừng lo lắng, sau ${output.timetowait} bạn có thể nhận daily!`)
+    		}
+ 
+  		}
+	  if (command === 'leaderboard') {
+ 
+    
+    if (message.mentions.users.first()) {
+ 
+      var output = await eco.Leaderboard({
+        filter: x => x.balance > 50,
+        search: message.mentions.users.first().id
+      })
+      message.channel.send(`Người chơi ${message.mentions.users.first().tag} nằm ở vị thứ ${output} trên bảng danh vọng!`);
+ 
+    } else {
+ 
+      eco.Leaderboard({
+        limit: 3, //Only takes top 3 ( Totally Optional )
+        filter: x => x.balance > 50 //Only allows people with more than 100 balance ( Totally Optional )
+      }).then(async users => { //make sure it is async
+ 
+        if (users[0]) var firstplace = await client.fetchUser(users[0].userid) //Searches for the user object in discord for first place
+        if (users[1]) var secondplace = await client.fetchUser(users[1].userid) //Searches for the user object in discord for second place
+        if (users[2]) var thirdplace = await client.fetchUser(users[2].userid) //Searches for the user object in discord for third place
+ 
+        message.channel.send(`My leaderboard:
+ 
+1 - ${firstplace && firstplace.tag || 'Chưa có ai'} : ${users[0] && users[0].balance || 'None'}
+2 - ${secondplace && secondplace.tag || 'Chưa có ai'} : ${users[1] && users[1].balance || 'None'}
+3 - ${thirdplace && thirdplace.tag || 'Chưa có ai'} : ${users[2] && users[2].balance || 'None'}`)
+ 
+      })
+ 
+    }
+  }
 })
 
-bot.on('ready', () => {
-	console.log('Khởi chạy tiền tệ...');	
-})
 
 bot.login(token);
