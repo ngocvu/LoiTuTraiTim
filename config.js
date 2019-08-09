40160b7c7b7600c622f178f8f1f54c2fde7db92b
lenhattruong165 <46161374+lenhattruong165@users.noreply.github.com>
2019-08-09 17:22:48 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = 'NjA4MzQyMjcxMzgwNjg0ODUx.XUy_Qg.8qojrKheLltyD8dcWCJy6zqqaUQ';
+exports.TOKEN = '';
 
 exports.PREFIX = 'boh';
 
-exports.GOOGLE_API_KEY = "AIzaSyDFGeaV3gO31FaSDDpIcFXo3Pj2l162bc0";
+exports.GOOGLE_API_KEY = "";
